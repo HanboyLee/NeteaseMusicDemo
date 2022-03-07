ece9f7fe5d80b005633d729893c642ea31ad9873
HanboyLee <50396232+HanboyLee@users.noreply.github.com>
2022-03-07 18:25:46 +0800

    測試

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://netease-cloud-api-sigma.vercel.app/";
+const BASE_URL = "https://netease-cloud-api-sigma.vercel.app";
 export const API_URL = process.env.NODE_ENV === "development" ? "/api" : BASE_URL;
 
 export const API_TIMEOUT = 40000;
